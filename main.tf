44c735a95d571f67a74396eb2b69d34eda096c6b
vasylky <vasylqvky@gmail.com>
2025-04-06 00:51:59 +0300

    fix variables and backend config

--- main.tf
+++ main.tf
@@ -13,17 +13,17 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "bestrong-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = "eastus"
+  name                = "${var.environment}-bestrong-vnet"
+  address_space       = var.vnet_address_space
+  location            = var.location
   resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "integration_subnet" {
-  name                 = "integration-subnet"
+  name                 = "${var.environment}bestrong_plan"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = var.subnet_address_prefix
   service_endpoints    = ["Microsoft.KeyVault"]
 
   delegation {
@@ -36,11 +36,11 @@ resource "azurerm_subnet" "integration_subnet" {
   }
 }
 
-resource "azurerm_key_vault" "kv123" {
-  name                      = "bestrong-keyvault121"
-  location                  = "eastus"
+resource "azurerm_key_vault" "keyvvv444" {
+  name                      = "${var.environment}-keyvvv4544"
+  location                  = var.location
   resource_group_name       = var.resource_group_name
-  sku_name                  = "standard"
+  sku_name                  = var.key_vault_sku
   tenant_id                 = data.azurerm_client_config.current.tenant_id
   enable_rbac_authorization = false
 
@@ -52,24 +52,24 @@ resource "azurerm_key_vault" "kv123" {
 }
 
 resource "azurerm_service_plan" "service_plan" {
-  name                = "bestrong-plan"
-  location            = "eastus"
+  name                = "${var.environment}-bestrong-plan"
+  location            = var.location
   resource_group_name = var.resource_group_name
   os_type             = "Windows"
-  sku_name            = "S1"
+  sku_name            = var.app_service_sku
 }
 
 resource "azurerm_application_insights" "insights" {
-  name                = "bestrong-insights"
-  location            = "eastus"
+  name                = "${var.environment}-bestrong-insights"
+  location            = var.location
   resource_group_name = var.resource_group_name
   application_type    = "web"
-  retention_in_days   = 90
+  retention_in_days   = var.app_insights_retention
 }
 
 resource "azurerm_windows_web_app" "app" {
-  name                = "bestrong-app"
-  location            = "eastus"
+  name                = "${var.environment}-bestrong-app"
+  location            = var.location
   resource_group_name = var.resource_group_name
   service_plan_id     = azurerm_service_plan.service_plan.id
 
@@ -80,12 +80,12 @@ resource "azurerm_windows_web_app" "app" {
   site_config {
     application_stack {
       current_stack  = "dotnet"
-      dotnet_version = "v6.0"
+      dotnet_version = var.dotnet_version
     }
   }
 
   app_settings = {
-    "KEY_VAULT_URL"                              = azurerm_key_vault.kv123.vault_uri
+    "KEY_VAULT_URL"                              = azurerm_key_vault.keyvvv444.vault_uri
     "APPINSIGHTS_INSTRUMENTATIONKEY"             = azurerm_application_insights.insights.instrumentation_key
     "APPLICATIONINSIGHTS_CONNECTION_STRING"      = azurerm_application_insights.insights.connection_string
     "ApplicationInsightsAgent_EXTENSION_VERSION" = "~2"
@@ -98,9 +98,9 @@ resource "azurerm_app_service_virtual_network_swift_connection" "vnet_integratio
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "bestrongacr"
+  name                = "${var.environment}bestrongacr"
   resource_group_name = var.resource_group_name
-  location            = "eastus"
+  location            = var.location
   sku                 = "Basic"
   admin_enabled       = false
 }
@@ -108,9 +108,9 @@ resource "azurerm_container_registry" "acr" {
 resource "azurerm_storage_account" "tfstate" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
-  location                 = "eastus"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  location                 = var.location
+  account_tier             = var.storage_account_tier
+  account_replication_type = var.storage_account_replication
 
   min_tls_version = "TLS1_2"
 }
@@ -121,12 +121,7 @@ resource "azurerm_storage_container" "tfstate" {
   container_access_type = "private"
 }
 
-resource "azurerm_management_lock" "tfstate_lock" {
-  name       = "lock-tfstate-storage"
-  scope      = azurerm_storage_account.tfstate.id
-  lock_level = "CanNotDelete"
-  notes      = "Lock for Terraform state storage account"
-}
+
 
 output "managed_identity_principal_id" {
   value       = azurerm_windows_web_app.app.identity[0].principal_id
--- variables.tf
+++ variables.tf
@@ -13,8 +13,72 @@ variable "storage_account_name" {
   type        = string
 }
 
+variable "storage_account_tier" {
+  description = "Storage account tier"
+  type        = string
+  default     = "Standard"
+}
+
+variable "storage_account_replication" {
+  description = "Storage account replication strategy"
+  type        = string
+  default     = "LRS"
+}
+
 variable "container_name" {
   description = "Azure Blob Container name"
   type        = string
 
+}
+
+variable "location" {
+  description = "location"
+  type        = string
+}
+
+variable "app_service_sku" {
+  description = "SKU for the App Service Plan"
+  type        = string
+  default     = "S1"
+}
+
+variable "dotnet_version" {
+  description = ".NET version for the web app"
+  type        = string
+  default     = "v6.0"
+}
+
+variable "key_vault_sku" {
+  description = "SKU for Key Vault"
+  type        = string
+  default     = "standard"
+}
+
+variable "acr_sku" {
+  description = "SKU for Azure Container Registry"
+  type        = string
+  default     = "Basic"
+}
+
+variable "app_insights_retention" {
+  description = "Retention in days for Application Insights data"
+  type        = number
+  default     = 90
+}
+
+variable "vnet_address_space" {
+  description = "Address space for the virtual network"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_address_prefix" {
+  description = "Address prefix for the integration subnet"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+variable "environment" {
+  description = "Environment name (e.g., dev, test, prod)"
+  type        = string
 }
