6f7dd8d67b60478853720a8a55dd301df9ce915a
vasylky <vasylqvky@gmail.com>
2025-04-13 13:04:08 +0300

    change outputs

--- outputs.tf
+++ outputs.tf
@@ -13,10 +13,6 @@ output "sql_server_fqdn" {
   description = "The fully qualified domain name of the SQL server"
 }
 
-output "storage_account_name" {
-  value       = module.storage.storage_account_name
-  description = "Name of the main storage account"
-}
 
 output "acr_login_server" {
   value       = module.container_registry.acr_login_server
