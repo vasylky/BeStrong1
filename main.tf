dd579c02bee4622daae72cab06702b4cadb708c2
vasylky <vasylqvky@gmail.com>
2025-04-13 15:42:13 +0300

    add credentials

--- main.tf
+++ main.tf
@@ -6,9 +6,6 @@ provider "azurerm" {
   features {}
   skip_provider_registration = true
   subscription_id            = var.subscription_id
-  client_id                  = var.client_id
-  client_secret              = var.client_secret
-  tenant_id                  = var.tenant_id
 }
 
 resource "random_id" "suffix" {
