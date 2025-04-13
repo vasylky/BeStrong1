cad4accb84fa499fe3a3e058d952316c7cfe2b96
vasylky <vasylqvky@gmail.com>
2025-04-13 12:55:11 +0300

    fix bugs

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -1,12 +1,3 @@
-output "storage_account_id" {
-  description = "ID of the main storage account"
-  value       = azurerm_storage_account.tfstate.id
-}
-
-output "storage_account_name" {
-  description = "Name of the main storage account"
-  value       = azurerm_storage_account.tfstate.name
-}
 
 output "app_storage_account_id" {
   description = "ID of the app storage account"
